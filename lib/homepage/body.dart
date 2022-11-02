628d7f0cc4a687f7105f503513da6f732df61e36
Vagas-Fácil <phlferezin03@gmail.com>
2022-11-02 13:58:43 -0300

    campos - form login

--- lib/homepage/body.dart
+++ lib/homepage/body.dart
@@ -1,4 +1,3 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:bordered_text/bordered_text.dart';
 import 'package:flutter/material.dart';
 
--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -11,36 +11,63 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(color: Colors.black),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Image.asset(
-              'assets/logo.png',
-              width: 150,
-              height: 150,
-            ),
-            Column(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                const Text(
-                  'Faça já o seu login',
-                  style: TextStyle(fontSize: 20),
+                Image.asset(
+                  'assets/logo.png',
+                  width: 150,
+                  height: 150,
                 ),
+                const Padding(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: Text(
+                      'Faça já o seu login',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
                 Container(
                   width: 250,
-                  height: 250,
+                  height: 200,
                   decoration: const BoxDecoration(color: Colors.white),
-                  child: Form(
-                    child: TextFormField(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        child: TextFormField(
+                          style: const TextStyle(color: Colors.black),
+                          decoration: const InputDecoration(
+                              hintText: "E-mail",
+                              hintStyle: TextStyle(color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1, color: Colors.black))),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 200,
+                        child: TextFormField(
+                          style: const TextStyle(color: Colors.black),
+                          decoration: const InputDecoration(
+                              hintText: "Senha",
+                              hintStyle: TextStyle(color: Colors.black),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1, color: Colors.black))),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ),
+                      
+                    ],
                   ),
-                )
+                ),
               ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
